2c5e8718d854daddf4cdee1b4041c166aa060365
JJ Ben-Joseph <jbenjoseph@iqt.org>
2020-10-13 23:13:04 -0400

    Add new columns.

--- worker/keter/data.py
+++ worker/keter/data.py
@@ -3,5 +3,8 @@ import pandas as pd
 from sqlalchemy.engine import Connectable
 
 def get_smiles_from_chembl(conn: Connectable) -> pd.DataFrame:
-    query = 'select canonical_smiles as smiles from compound_structures'
+    query = 'select canonical_smiles as smiles, ' \
+            'standard_inchi as inchi, ' \
+            'standard_inchi_key as inchi_key ' \
+            'from compound_structures'
     return pd.read_sql(query, conn)
