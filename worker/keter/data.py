87407c9467037404f4bbb25127de0c52ceb434eb
JJ Ben-Joseph <jbenjoseph@iqt.org>
2020-11-04 18:34:34 -0500

    Add new dataset.

--- worker/keter/data.py
+++ worker/keter/data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from functools import reduce
+import pandas as pd
 
 
 def gather_mols_with_props() -> pd.DataFrame:
@@ -11,9 +12,22 @@ def gather_mols_with_props() -> pd.DataFrame:
         "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/BBBP.csv",
         "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/pcba.csv.gz",
         "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/muv.csv.gz",
+        lambda: pd.read_csv(
+            "https://github.com/molecularsets/moses/raw/master/data/dataset_v1.csv",
+            names=["smiles", "partition"],
+            header=None,
+            usecols=["smiles"],
+        ),
     ]
+
+    def read_csv(csv) -> pd.DataFrame:
+        if isinstance(csv, str):
+            return pd.read_csv(csv)
+        else:
+            return csv()
+
     result = reduce(
         lambda left, right: pd.merge(left, right, on="smiles", how="outer", sort=False),
-        (pd.read_csv(csv) for csv in urls),
+        (read_csv(csv) for csv in urls),
     )
     return result[result.smiles != "FAIL"]
