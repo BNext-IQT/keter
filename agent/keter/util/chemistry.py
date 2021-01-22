bdc7b83cabeff0548502ae5c085aafe8c443af18
JJ Ben-Joseph <jbenjoseph@iqt.org>
2021-01-22 16:35:47 -0500

    Add faster function for collections of SMILES.

--- agent/keter/util/chemistry.py
+++ agent/keter/util/chemistry.py
@@ -1,5 +1,12 @@
+from typing import Sequence
 from rdkit import Chem
 
 
 def smiles2key(smiles: str) -> str:
-    return Chem.inchi.MolToInchiKey(Chem.MolFromSmiles(smiles))
+    return Chem.MolToInchiKey(Chem.MolFromSmiles(smiles))
+
+
+def smiles2keys(smiles: Sequence) -> Sequence:
+    mols = Chem.SmilesMolSupplierFromText("\n".join(smiles), " ", 0, -1, 0)
+    for mol in mols:
+        yield Chem.MolToInchiKey(mol)
