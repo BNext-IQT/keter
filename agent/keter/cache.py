59e21afec9e870aa45cf66b4f5455c40447545fe
JJ Ben-Joseph <jbenjoseph@iqt.org>
2021-02-19 13:32:41 -0500

    Make CACHE_ROOT configurable via env variable.

--- agent/keter/cache.py
+++ agent/keter/cache.py
@@ -1,7 +1,10 @@
+import os
 from pathlib import Path
 from typing import Sequence
 from dvc.repo import Repo
 
-CACHE_ROOT = (Path(__file__).parent.parent / "cache").absolute()
+CACHE_ROOT = (
+    os.environ.get("KETER_CACHE") or (Path(__file__).parent.parent / "cache").absolute()
+)
 
 
