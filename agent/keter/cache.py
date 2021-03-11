4087a75e8e406d022122607afdc983abfbe99427
JJ Ben-Joseph <jbenjoseph@iqt.org>
2021-03-10 21:39:07 -0500

    Add new cache function.

--- agent/keter/cache.py
+++ agent/keter/cache.py
@@ -1,6 +1,8 @@
 import os
+import pickle
 from pathlib import Path
-from typing import Sequence
+from typing import Sequence, Callable
+import lzma
 from dvc.repo import Repo
 
 CACHE_ROOT = (
@@ -11,3 +13,15 @@ MODEL_ROOT = CACHE_ROOT / "models"
 OUTPUTS_ROOT = CACHE_ROOT / "outputs"
 
 
+def cache(filename: Path, func: Callable, mode="b"):
+    path = filename.with_suffix(".pkz")
+
+    if path.exists():
+        with lzma.open(path, "r" + mode) as fd:
+            return pickle.load(fd)
+    else:
+        obj = func()
+        path.parents[0].mkdir(parents=True, exist_ok=True)
+        with lzma.open(path, "w" + mode) as fd:
+            pickle.dump(obj, fd)
+        return obj
