2e0ad039dece62d6f18e81741b2bbbdad3869972
JJ Ben-Joseph <jbenjoseph@iqt.org>
2021-03-10 21:55:08 -0500

    Use new cache function.

--- agent/keter/actors/vectors.py
+++ agent/keter/actors/vectors.py
@@ -1,40 +1,42 @@
 import pickle
 import lzma
+from typing import Sequence
 from keter.models.vectors import ChemicalLanguageModule, ChemicalLanguageHyperparameters
 from keter.datasets.constructed import Unlabeled, Toxicity
-from keter.cache import MODEL_ROOT
+from keter.cache import MODEL_ROOT, cache
 
 
 class ChemicalLanguage:
     filename = "chemical_language"
 
     def __init__(self, mode="default"):
-        model_file = (MODEL_ROOT / self.filename).with_suffix(f".{mode}.pickle.xz")
-        if model_file.exists():
-            with lzma.open(model_file, "rb") as fd:
-                self.model = pickle.load(fd)
-            return
+        model_file = MODEL_ROOT / f"{self.filename}_{mode}"
 
         if mode == "default":
-            self.model = self.train()
+            self.model = cache(model_file, self.train)
         elif mode == "bow":
-            self.model = self.train(
-                ChemicalLanguageHyperparameters.from_dict({"vector_algo": "bow"})
+            self.model = cache(
+                model_file,
+                lambda: self.train(
+                    ChemicalLanguageHyperparameters.from_dict({"vector_algo": "bow"})
+                ),
             )
         elif mode == "lda":
-            self.model = self.train(
-                ChemicalLanguageHyperparameters.from_dict({"vector_algo": "lda"})
+            self.model = cache(
+                model_file,
+                lambda: self.train(
+                    ChemicalLanguageHyperparameters.from_dict({"vector_algo": "lda"})
+                ),
             )
         elif mode == "fastd2v":
-            self.model = self.train(
-                ChemicalLanguageHyperparameters.from_dict({"doc_epochs": 5})
+            self.model = cache(
+                model_file,
+                lambda: self.train(
+                    ChemicalLanguageHyperparameters.from_dict({"doc_epochs": 5})
+                ),
             )
         else:
             raise ValueError("Invalid mode: " + mode)
-
-        MODEL_ROOT.mkdir(parents=True, exist_ok=True)
-        with lzma.open(model_file, "wb") as fd:
-            pickle.dump(self.model, fd)
 
     def train(self, hyperparams=ChemicalLanguageHyperparameters()):
         tox = Toxicity().to_df(cache=True)
@@ -44,3 +46,6 @@ class ChemicalLanguage:
         model = ChemicalLanguageModule(hyperparams)
         model.fit(Unlabeled().to_list(cache=True), X, y)
         return model
+
+    def transform(self, smiles: Sequence[str]) -> Sequence[str]:
+        return self.model.to_vecs(smiles)
