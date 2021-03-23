16ed90c53de2ddb2c5d762d7fdb79d275f418088
JJ Ben-Joseph <jbenjoseph@iqt.org>
2021-03-23 16:49:59 -0400

    Add Feasibility model to analyzer.

--- agent/keter/actors/sklearn.py
+++ agent/keter/actors/sklearn.py
@@ -3,7 +3,7 @@ from autosklearn.estimators import AutoSklearnRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
-from keter.datasets.constructed import Toxicity
+from keter.datasets.constructed import Toxicity, Feasibility
 from keter.datasets.raw import Tox21
 from keter.actors.vectors import ChemicalLanguage
 from keter.cache import cache, MODEL_ROOT
@@ -16,7 +16,7 @@ class Analyzer:
         model_file = MODEL_ROOT / f"{self.filename}_{mode}"
         self.preprocessor = ChemicalLanguage("bow")
         if mode == "prod":
-            self.safety_model, self.synth_model = cache(model_file, self.train)
+            self.safety_model, self._model = cache(model_file, self.train)
         elif mode == "test":
             self.safety_model, self.synth_model = self.train(
                 score=True, task_duration=300
@@ -26,28 +26,30 @@ class Analyzer:
 
     def train(self, score=False, task_duration=28800):
         def train_model(data, target_label):
-            data = Toxicity().to_df(cache=True)
+            dataframe = data.to_df(cache=True)
             model = AutoSklearnRegressor(time_left_for_this_task=task_duration)
 
             if score:
                 Xt, Xv, yt, yv = train_test_split(
-                    self.preprocessor.transform(data["smiles"]),
-                    data[target_label],
+                    self.preprocessor.transform(dataframe["smiles"]),
+                    dataframe[target_label],
                     test_size=0.15,
                     random_state=18,
                 )
             else:
-                Xt = self.preprocessor.transform(data["smiles"])
-                yt = data[target_label]
+                Xt = self.preprocessor.transform(dataframe["smiles"])
+                yt = dataframe[target_label]
 
             model.fit(Xt, yt)
 
             if score:
-                print(f"Score: {model.score(Xv, yv)}")
+                print(f"Score on {target_label}: {model.score(Xv, yv)}")
 
             return model
 
-        return train_model(Toxicity(), "toxicity"), None
+        return train_model(Toxicity(), "toxicity"), train_model(
+            Feasibility(), "feasibility"
+        )
 
     def analyze(self, smiles: Sequence[str]) -> Sequence[float]:
         return self.safety_model.predict(self.preprocessor.transform(smiles))
