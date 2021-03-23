54c97b6ffea6b2135f6c9f02d81ebeaa39f55795
JJ Ben-Joseph <jbenjoseph@iqt.org>
2021-03-22 23:41:23 -0400

    Share code between models.

--- agent/keter/actors/sklearn.py
+++ agent/keter/actors/sklearn.py
@@ -25,28 +25,29 @@ class Analyzer:
             raise ValueError(f"Invalid mode: {mode}")
 
     def train(self, score=False, task_duration=28800):
-        tox_data = Toxicity().to_df(cache=True)
+        def train_model(data, target_label):
+            data = Toxicity().to_df(cache=True)
+            model = AutoSklearnRegressor(time_left_for_this_task=task_duration)
 
-        safety_model = AutoSklearnRegressor(time_left_for_this_task=task_duration)
-        synth_model = AutoSklearnRegressor(time_left_for_this_task=task_duration)
+            if score:
+                Xt, Xv, yt, yv = train_test_split(
+                    self.preprocessor.transform(data["smiles"]),
+                    data[target_label],
+                    test_size=0.15,
+                    random_state=18,
+                )
+            else:
+                Xt = self.preprocessor.transform(data["smiles"])
+                yt = data[target_label]
 
-        if score:
-            Xt, Xv, yt, yv = train_test_split(
-                self.preprocessor.transform(tox_data["smiles"]),
-                tox_data["toxicity"],
-                test_size=0.15,
-                random_state=18,
-            )
-        else:
-            Xt = self.preprocessor.transform(tox_data["smiles"])
-            yt = tox_data["toxicity"]
+            model.fit(Xt, yt)
 
-        safety_model.fit(Xt, yt)
+            if score:
+                print(f"Score: {model.score(Xv, yv)}")
 
-        if score:
-            print(f"Score: {safety_model.score(Xv, yv)}")
+            return model
 
-        return safety_model, synth_model
+        return train_model(Toxicity(), "toxicity"), None
 
     def analyze(self, smiles: Sequence[str]) -> Sequence[float]:
         return self.safety_model.predict(self.preprocessor.transform(smiles))
